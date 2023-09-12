fb2aba8746519624b92ed3e8cafa8a4e90692333
slmnfqh <jhokamfaqih5@gmail.com>
2023-09-12 20:01:24 +0700

    first commit belajar docker

---
+++ index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World, Saya sedang belajar docker!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
